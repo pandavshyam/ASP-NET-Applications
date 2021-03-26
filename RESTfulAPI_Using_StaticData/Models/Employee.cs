887b9f8d3deef85f3eeef630cf08f55a55c6fa29
Shyam Pandav <46902914+pandavshyam@users.noreply.github.com>
2021-03-27 02:00:54 +0530

    Add files via upload

---
+++ RESTfulAPI_Using_StaticData/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RESTfulAPI.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Adress { get; set; }
+
+
+        public List<Employee> GetEMps()
+        {
+            List<Employee> emps = new List<Employee>();
+            emps.Add(new Employee() { Id = 10, Name = "Sachin", Adress = "Mumbai" });
+            emps.Add(new Employee() { Id = 11, Name = "Virat", Adress = "Delhi" });
+            emps.Add(new Employee() { Id = 12, Name = "Ajinkya", Adress = "Pune" });
+
+            return emps;
+        }
+    }
+}
